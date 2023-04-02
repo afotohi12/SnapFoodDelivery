7e03646064e7194ed3351560984d80866d908ba5
afotohi12 <afotohi12@hotmail.com>
2023-04-02 13:41:18 +0330

    resController

--- validation/checkLogin.js
+++ validation/checkLogin.js
@@ -1,13 +1,12 @@
-const {compToken} = require('../module/encrypt');
+const { compToken } = require('../module/encrypt');
 //بررسی لاگین بودن کاربر
-const checkLogin = (req,res,next) => {
+const checkLogin = (req, res, next) => {
     try {
         const user = compToken(req.headers.authorization.slice(7));
         req.userName = user.date;
-        
         next();
     } catch (error) {
-        next({status : 401,message : "Please Login"});
+        next({ status: 401, message: "Please Login" });
     }
 };
 
