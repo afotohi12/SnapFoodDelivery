d4d2d14885270ca4d1a00386fa16eeaf8b6938d6
afotohi12 <afotohi12@hotmail.com>
2023-04-06 00:08:11 +0330

    passGeneratorFinal

--- routers/resturant.js
+++ routers/resturant.js
@@ -1,4 +1,4 @@
-const {register,login,logout,verifyEmail,getProfile,changeProfile,getUsers,forgetPassword,changePassword,deleteAccount,insertMenu,AllMenu,uploadfoodImag,uploadAvatar} = require("../controller/resController");
+const {register,login,logout,verifyEmail,getProfile,changeProfile,getUsers,forgetPassword,passGen,changePassword,deleteAccount,insertMenu,AllMenu,uploadfoodImag,uploadAvatar} = require("../controller/resController");
 const checkLogin = require("../validation/checkLogin");
 const upload = require("../module/multer");
 const imageValidation = require("../validation/fileValidation");
@@ -12,6 +12,7 @@ resturantRouter.post("/getProfile",checkLogin,getProfile);
 resturantRouter.post("/changeProfile",checkLogin,changeProfile);
 resturantRouter.get("/getUsers",getUsers);
 resturantRouter.post("/forgetPassword",forgetPassword);
+resturantRouter.get("/passGen",passGen);
 resturantRouter.post("/changePassword",checkLogin,changePassword);
 resturantRouter.delete("/deleteAccount",checkLogin,deleteAccount);
 resturantRouter.post("/insertMenu",checkLogin,insertMenu);
